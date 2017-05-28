a795644d8137ab9226d74328352c81252232a1f8
葫芦娃八娃 <mojotintin@outlook.com>
2017-05-28 11:50:21 +0800

    add the watchlist

--- boosting.py
+++ boosting.py
@@ -1,17 +1,17 @@
-import numpy as np
 import pandas as pd
 import xgboost as xgb
-import time
-
-# 记录程序运行时间
-start_time = time.time()
 
 #  划分数据集
 train_frame = pd.read_csv('data/new_train.csv')
 test_frame = pd.read_csv('data/new_test.csv')
-X_train = train_frame.iloc[:, 2:-1]
-Y_train = train_frame.iloc[:, -1:].values
+offset = 600
+X_train = train_frame.iloc[:offset, 2:-1]
+Y_train = train_frame.iloc[:offset, -1:].values
+
+X_val = train_frame.iloc[offset:, 2:-1]
+Y_val = train_frame.iloc[offset:, -1:].values
 X_test = test_frame.iloc[:, 2:]
+
 
 # 模型参数设置
 param = {'booster': 'gbtree',
@@ -21,11 +21,13 @@ param = {'booster': 'gbtree',
          'normalize_type': 'tree',
          'rate_drop': 0.1,
          'skip_drop': 0.5}
-num_round = 50
-
+num_round = 200
 #  进行预测
 xg_train = xgb.DMatrix(X_train[:], label=Y_train[:])
-bst = xgb.train(param, xg_train, num_round)
+xg_val = xgb.DMatrix(X_val[:], label=Y_val[:])
+watchlist = [(xg_train, 'train'), (xg_val, 'val')]
+
+bst = xgb.train(param, xg_train, num_round, watchlist)
 xg_test = xgb.DMatrix(X_test)
 Y_test = bst.predict(xg_test)
 
@@ -39,7 +41,5 @@ def convert_re(survived):
 sur = pd.Series(Y_test)
 
 re_frame = pd.read_csv('data/gender_submission.csv')
-
 re_frame['Survived'] = sur.map(convert_re)
-
 re_frame.to_csv('data/re.csv', index=False)
